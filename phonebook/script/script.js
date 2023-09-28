27851dd5a3e0619b5dce3ed058b4331511aac876
Dima163163 <khlopovdima@gmail.com>
2023-09-28 14:35:04 +0400

    DOM4 PhoneBook

---
+++ phonebook/script/script.js
@@ -0,0 +1,246 @@
+'use strict';
+
+// Данные для теста
+const data = [
+  {
+    name: 'Иван',
+    surname: 'Петров',
+    phone: '+79514545454',
+  },
+  {
+    name: 'Игорь',
+    surname: 'Семёнов',
+    phone: '+79999999999',
+  },
+  {
+    name: 'Семён',
+    surname: 'Иванов',
+    phone: '+79800252525',
+  },
+  {
+    name: 'Мария',
+    surname: 'Попова',
+    phone: '+79876543210',
+  },
+];
+
+{
+  // Функциф создания Container
+  const createContainer = () => {
+    const container = document.createElement('div');
+    container.classList.add('container');
+    return container;
+  };
+
+  // Функция создания header
+  const createHeader = () => {
+    const header = document.createElement('header');
+    header.classList.add('header');
+    const headerContainer = createContainer();
+    header.append(headerContainer);
+
+    header.headerContainer = headerContainer;
+
+    return header;
+  };
+  // Функция создания логотипа
+  const createLogo = (title) => {
+    const h1 = document.createElement('h1');
+    h1.classList.add('logo');
+    h1.textContent = `Телефонный справочник. ${title}`;
+
+    return h1;
+  };
+
+  // Функция создания main секции
+  const createMain = () => {
+    const main = document.createElement('main');
+    main.classList.add('main');
+    const mainContainer = createContainer();
+
+    main.append(mainContainer);
+    main.mainContainer = mainContainer;
+    return main;
+  };
+
+  // Функция создания кнопок
+  const createButtonGroup = (params) => {
+    const btnWrapper = document.createElement('div');
+    btnWrapper.classList.add('btn-wrapper');
+
+    const btns = params.map(({className, type, text}) => {
+      const button = document.createElement('button');
+      button.type = type;
+      button.textContent = text;
+      button.className = className;
+
+      return button;
+    });
+
+    btnWrapper.append(...btns);
+
+    return {
+      btnWrapper,
+      btns,
+    };
+  };
+
+  // Функция создания таблицы
+  const createTable = () => {
+    const table = document.createElement('table');
+    table.classList.add('table', 'table-striped');
+    const thead = document.createElement('thead');
+    thead.insertAdjacentHTML('beforeend', `
+    <tr>
+    <th class='delete'>Удалить</th>
+    <th>Имя</th>
+    <th>Фамилия</th>
+    <th>Телефон</th>
+    </tr>
+    `);
+    const tbody = document.createElement('tbody');
+    table.append(thead, tbody);
+    table.tbody = tbody;
+
+    return table;
+  };
+
+  // Функция создания формы
+  const createForm = () => {
+    const overlay = document.createElement('div');
+    overlay.classList.add('form-overlay');
+
+    const form = document.createElement('form');
+    form.classList.add('form');
+    form.insertAdjacentHTML('beforeend', `
+      <button class="close" type="button"></button>
+      <h2 class="form-title">Добавить контакт</h2>
+      <div class="form-group">
+      <label class="form-label" for="name">Имя:</label>
+      <input class="form-input" name="name" id="name" type="text" requared>
+      </div>
+      <div class="form-group">
+      <label class="form-label" for="surname">Фамилия:</label>
+      <input class="form-input" name="surname"
+      id="surname" type="text" requared>
+      </div>
+      <div class="form-group">
+      <label class="form-label" for="phone">Телефон:</label>
+      <input class="form-input" name="phone" id="phone" type="number" requared>
+      </div>
+    `);
+    const buttonGroup = createButtonGroup([
+      {
+        className: 'btn btn-primary mr-3',
+        type: 'submit',
+        text: 'Добавить',
+      },
+      {
+        className: 'btn btn-danger',
+        type: 'reset',
+        text: 'Отмена',
+      },
+    ]);
+    form.append(...buttonGroup.btns);
+    overlay.append(form);
+
+    return {
+      overlay,
+      form,
+    };
+  };
+
+  // Функция создания footer секции
+  const createFooter = () => {
+    const footer = document.createElement('footer');
+    footer.classList.add('footer');
+
+    const footerContainer = createContainer();
+    footer.append(footerContainer);
+    footer.footerContainer = footerContainer;
+
+    return footer;
+  };
+  // Функция создания логотипа footer
+  const createLogoFooter = (title) => {
+    const h2 = document.createElement('h2');
+    h2.classList.add('logo-footer');
+    h2.style.cssText = `font-size: 20px;
+    font-weight: 400;`;
+    h2.textContent = `Все права защищены ©${title}`;
+
+    return h2;
+  };
+
+  const renderPhoneBook = (app, title) => {
+    const header = createHeader();
+    const logo = createLogo(title);
+    const main = createMain();
+    const buttonGroup = createButtonGroup([
+      {
+        className: 'btn btn-primary mr-3',
+        type: 'button',
+        text: 'Добавить',
+      },
+      {
+        className: 'btn btn-danger',
+        type: 'button',
+        text: 'Удалить',
+      },
+    ]);
+    const table = createTable();
+    const form = createForm();
+    const footer = createFooter();
+    const logoFooter = createLogoFooter(title);
+
+    header.headerContainer.append(logo);
+    main.mainContainer.append(buttonGroup.btnWrapper, table, form.overlay);
+    footer.footerContainer.append(logoFooter);
+
+    app.append(header, main, footer);
+    return {
+      list: table.tbody,
+    };
+  };
+
+  // Функция создания строк с контактами на основе полученных данных
+  const createRow = ({name: firstName, surname, phone}) => {
+    const tr = document.createElement('tr');
+
+    const tdDel = document.createElement('td');
+    tdDel.classList.add('delete');
+    const buttonDel = document.createElement('button');
+    buttonDel.classList.add('del-icon');
+    tdDel.append(buttonDel);
+
+    const tdName = document.createElement('td');
+    tdName.textContent = firstName;
+    const tdSurname = document.createElement('td');
+    tdSurname.textContent = surname;
+    const tdPhone = document.createElement('td');
+    const phoneLink = document.createElement('a');
+    phoneLink.href = `tel:${phone}`;
+    phoneLink.textContent = phone;
+
+    tdPhone.append(phoneLink);
+    tr.append(tdDel, tdName, tdSurname, tdPhone);
+
+    return tr;
+  };
+
+  // Фунция отрисовки контактов
+  const renderContacts = (elem, data) => {
+    const allRow = data.map(createRow);
+    elem.append(...allRow);
+  };
+
+  const init = (selectorApp, title) => {
+    const app = document.querySelector(selectorApp);
+    const phoneBook = renderPhoneBook(app, title);
+    const {list} = phoneBook;
+    renderContacts(list, data);
+    // Функционал
+  };
+
+  window.phoneBookInit = init;
+}
